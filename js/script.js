f03c189c14d5b094492fb374a414d794152e3de4
thisissteve <skl348@nyu.edu>
2016-03-27 19:54:10 -0400

    final version with randomness and dog/cats

--- js/script.js
+++ js/script.js
@@ -52,6 +52,8 @@ function opClicked(e) {
     // equal sign op pressed
     else {
         // if op and operand selected, do calculation
+        // if addition operation is performed, adds that number of cats to page
+        // if subtraction operation is performed, adds that number of dogs to the page
         if (operator != '' && rightOperand != '')
         {
             var num = Number(rightOperand);
@@ -59,12 +61,18 @@ function opClicked(e) {
             {
                 console.log("adding now");
                 addNum(num);
+
+                // add (num) # of cats to the page
+
             }
 
             else if (operator === minusSignStr)
             {
                 console.log("subtracting now");
                 subtractNum(num);
+
+                // add (num) # of dogs to the page
+
             }
 
 
@@ -95,7 +103,48 @@ function addNum(num) {
     
     var result = outputVal + num;
 
+
     setEltToVal(outputElt, result);
+
+    if (outputVal >= 0)
+    {
+        // (num) MORE CATS!
+        addXAnimalsFromStartIdY(num, outputVal, 0);
+    }
+
+    // outputVal < 0
+    else
+    {
+        if (result < 0)
+        {
+            // remove num images (dogs)
+            for (var i = 0; i < num; i++)
+            {
+                var elem = document.getElementById("img" + i);
+                elem.remove();
+            }
+        }
+        else
+        {
+            // clear all imgs
+            var numDogs = Math.abs(outputVal);
+            for (var i = 0; i < numDogs; i++)
+            {
+                var elem = document.getElementById("img" + i);
+                elem.remove();
+            }
+            if (result > 0)
+            {
+                // (result) MORE CATS! id starts from 0
+                addXAnimalsFromStartIdY(result, 0, 0);
+            }
+        }
+    }
+
+
+
+
+    //addXCatsFromStartIdY(num, 0);
 }
  
 function subtractNum(num) {
@@ -105,6 +154,42 @@ function subtractNum(num) {
     var result = outputVal - num;
 
     setEltToVal(outputElt, result);
+
+    if (outputVal <= 0)
+    {
+        // (num) MORE DOGS!
+        addXAnimalsFromStartIdY(num, Math.abs(outputVal), 1);
+    }
+
+    // outputVal > 0
+    else
+    {
+        if (result > 0)
+        {
+            // remove num images (cats)
+            for (var i = 0; i < num; i++)
+            {
+                var elem = document.getElementById("img" + i);
+                elem.remove();
+            }
+        }
+        else
+        {
+            // clear all imgs
+            var numCats = Math.abs(outputVal);
+            for (var i = 0; i < numCats; i++)
+            {
+                var elem = document.getElementById("img" + i);
+                elem.remove();
+            }
+            if (result < 0)
+            {
+                // (result) MORE DOGS! id starts from 0
+                addXAnimalsFromStartIdY(Math.abs(result), 0, 1);
+            }
+        }
+    }
+
 }
 
 // sets innerhtml of passed in element to passed in value
@@ -122,16 +207,13 @@ function setEltToVal(elt, val) {
 
     /* add random pic to some random location on page*/
 
+    /*
     var randX = Math.random() * documentWidth();
     var randY = Math.random() * documentHeight();
     console.log('(' + randX, randY + ')');
 
-    var elem = document.createElement("img");
-    elem.setAttribute("width", "150px");
-    elem.setAttribute("alt", "cat 0");
-    elem.src = "img/0.svg";
-    elem.style.cssText = 'position:absolute; top:' + randY + 'px; left:' + randX + 'px; z-index:0;';
-    document.body.appendChild(elem);
+    addImgToDOM(randX, randY, "img/0.svg", 0);
+    */
 }
 
 
@@ -153,4 +235,41 @@ function documentWidth() {
         document.body.offsetWidth,
         document.documentElement.offsetWidth
     );
+}
+
+// creates an img element and adds it to a random location on the page
+// given a string for src attr of element and id (number) 
+// img tags will have id's of the form "img#" # being id #
+function addImgToDOM(x, y, srcStr, id) {
+    var elem = document.createElement("img");
+    
+    elem.setAttribute("width", "150px");
+    elem.setAttribute("alt", "im kawaii");
+    elem.setAttribute("id", "img" + id);
+    elem.src = srcStr;
+    elem.style.cssText = 'position:absolute; top:' + y + 'px; left:' + x + 'px; z-index:0;';
+    document.body.appendChild(elem);
+}
+
+// adds x imgs to the page with ids starting from y, increments per img placed
+// if animalType == 0 will place cats, if animalType == 1 will place dogs
+// for both types, have names [0-8].html
+function addXAnimalsFromStartIdY(x, y, animalType) {
+    var folderPath = "img/";
+    if (animalType == 0)
+        folderPath += "cats/";
+    else if (animalType == 1)
+        folderPath += "dogs/";
+    var currId = y;
+    for(var i = 0; i < x; i++)
+    {
+        var num = generateRandNum(0,8); // random num for image selection
+        var srcPath = folderPath + num + ".svg";
+
+        // generate random x,y for each cat
+        var randX = Math.random() * documentWidth();
+        var randY = Math.random() * documentHeight();
+        addImgToDOM(randX, randY, srcPath, currId);
+        currId++;
+    }
 }
